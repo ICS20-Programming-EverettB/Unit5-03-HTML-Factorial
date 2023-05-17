599c6167fdef7686c71d09e8c7169e7f3577a838
EverettBernard <124380095+EverettBernard@users.noreply.github.com>
2023-05-17 15:04:34 +0000

    did js

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Created by: Everett
+// Created on: May 2023
+// This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function multiples 2 numbers using for loops.
+ */
+function calculate () {
+  
+  // get usernum
+  let usernum = parseInt(document.getElementById('num').value);
+  
+  // display open string
+  let display= ""
+  
+  // declare variable for counter and factorial 
+  let counter = 0
+  let factorial = 1
+  
+  //if statment for if number is a negative 
+  if ( usernum <0 ) {
+     display = "Please input a positive integer."
+  }
+      
+  // else statment for do while loop
+  else {
+    
+  // do while loop for calculating facotrial of usernum 
+     do {
+    
+   factorial = factorial* (counter+1)
+    
+    counter = counter + 1
+    
+     } while (counter < usernum)
+    
+  //display variable for factorial 
+    display = factorial 
+  }
+ 
+  // output facttorial of user number to user
+  document.getElementById('display-multiply').innerHTML = display
+
+}
+ 
+
+ 
